9ba68ac61d6cb87a20f7b669fa7eb4a6908fc8fe
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-06 23:31:21 +0700

    tugas 2 javascript

---
+++ tugas2.js
@@ -0,0 +1,34 @@
+/* tugas 2 javascript */
+
+// tipe data number
+var number = -15;
+console.log(number);
+
+// tipe data string
+var string = "ini anak budi";
+console.log(string);
+
+// tipe data boolean
+var jauh = true;
+var km = 15;
+
+if(km>10) {
+    jauh = true;
+}else{
+    jauh = false;
+}
+
+console.log(jauh);
+
+// tipe data null
+var laper = "makan";
+laper = null;
+console.log(laper);
+
+// tipe data undifined
+var ayam;
+console.log(ayam);
+
+//tipe data array
+var tipe = [15,"pengen makan","burger",43,"true"];
+console.log(tipe[1]);
